8009ef5847c16182312b2083d4538f6e5c53375c
Andrew Davis <andrew.davis@kaltura.com>
2012-03-05 16:54:13 +0000

    adding simple svn to github sync
    git-svn-id: http://www.kaltura.org/kalorg/html5video/branches/git_svn_sync@4230 b58a29cf-3064-46da-94c6-1c29cc75c8e5

---
+++ sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+git svn fetch kaltura.org
+git checkout develop
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+git checkout 1.6.9
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+git checkout 1.6.8
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+git checkout firefogg
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+git checkout gh-pages
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+git checkout kplayer-examples
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+git checkout packager
+git svn rebase
+#git svn dcommit # uncomment to push back to svn
+
+git push kaltura --all
